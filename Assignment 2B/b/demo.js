ccccdafdea582707ad6011e81b6b99270525c66a
divyannshugiri <154437803+divyannshugiri@users.noreply.github.com>
2024-05-03 20:36:26 +0530

    Add files via upload

---
+++ Assignment 2B/b/demo.js
@@ -0,0 +1,5 @@
+console.log("Subject : Web Apllication Development");
+console.log("Name    : Divyanshu Giri ");
+console.log("Class   : 3rd Year");
+console.log("Branch  :  IT");
+console.log("Mail-id : divyanshugiri34@gmail.com");
